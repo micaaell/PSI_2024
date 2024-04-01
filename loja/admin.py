7e8ce8487621d718189636ee4427ebb750d622d6
micaaell <micael.ferreira@escolar.ifrn.edu.br>
2024-04-01 08:19:29 -0300

    atualização

--- loja/admin.py
+++ loja/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin #isso já vai estar existindo no arquivo
 # Register your models here.
 from .models import * #imporata nossos models
-admin.site.register(Fabricante) #adiciona a interface do adm
+
+class FabricanteAdmin(admin.ModelAdmin):
+# Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao','preco', 'categoria',)
+    empty_value_display = 'Vazio'
+admin.site.register(Fabricante,FabricanteAdmin)#adiciona a interface do adm
 admin.site.register(Categoria)
-admin.site.register(Produto)
+admin.site.register(Produto,ProdutoAdmin)
