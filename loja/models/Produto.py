a2b8a728194ea67407936e605da42ed4c16f9dad
micaaell <micael.ferreira@escolar.ifrn.edu.br>
2024-04-05 09:45:52 -0300

    dia 05 de abril

--- loja/models/Produto.py
+++ loja/models/Produto.py
@@ -8,10 +8,9 @@ class Produto(models.Model):
     preco = models.DecimalField(max_digits=8, decimal_places=2)
     categoria = models.ForeignKey(Categoria, null=True,related_name='categoria', on_delete=models.SET_NULL)
     fabricante = models.ForeignKey(Fabricante, null=True,related_name='fabricante', on_delete=models.SET_NULL)
-
-#loja/models/Produto.py parte 2:
-criado_em = models.DateTimeField(auto_now_add=True)
-alterado_em = models.DateTimeField(auto_now=True)
-image = models.ImageField(null=True, blank=True)
+    
+    criado_em = models.DateTimeField(auto_now_add=True)
+    alterado_em = models.DateTimeField(auto_now=True)
+    image = models.ImageField(null=True, blank=True)
 def __str__(self):
     return '{}'.format(self.Produto)
--- lojaAdmin/urls.py
+++ lojaAdmin/urls.py
@@ -20,8 +20,4 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
