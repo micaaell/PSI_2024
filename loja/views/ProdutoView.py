7c3b6937cc42062c103a455bd2a0675ca0d26ff8
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2024-08-08 01:45:30 +0000

    Atualizações com o HTML e css

--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render # Retire from django.http import HttpResponse
+from loja.models import Produto
 from loja.models import Produto
 
 from datetime import timedelta, datetime
@@ -30,6 +31,20 @@ def list_produto_view(request, id=None):
     if id is not None:
         produtos = produtos.filter(id=id)
     print(produtos)
+    # Adicione para definir o contexto e carregar o template
+    context = {
+    'produtos': produtos
+    }
+    return render(request, template_name='produto/produto.html',context=context, status=200)
     if id is None:
         return HttpResponse('<h1>Nenhum id foi informado</h1>')
     return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+        produto = produtos.first()
+        print(produto)
+        context = { 'produto': produto }
+        return render(request, template_name='produto/produto-edit.html', context=context,status=200)
