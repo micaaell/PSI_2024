4d20b930b7abc2297184542c7de8bdb34b94dd13
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2024-10-21 11:15:33 +0000

    Atualização do form parte final

--- loja/views/UsuarioView.py
+++ loja/views/UsuarioView.py
@@ -1,17 +1,43 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from loja.models import Usuario
-from loja.forms.UserUsuarioForm import UserUsuarioForm
+from loja.forms.UserUsuarioForm import UserUsuarioForm, UserForm
 def list_usuario_view(request, id=None):
     # carrega somente usuarios, não inclui os admin
     usuarios = Usuario.objects.filter(perfil=2)
-    context = {'usuarios': usuarios}
     return render(request, template_name='usuario/usuario.html', context=context,status=200)
 
 # adicione o método de edição
 def edit_usuario_view(request):
     usuario = get_object_or_404(Usuario, user=request.user)
+    emailUnused = True
+    message = None
+    if request.method == 'POST':
+        usuarioForm = UserUsuarioForm(request.POST, instance=usuario)
+        userForm = UserForm(request.POST, instance=request.user)
+        #context = {'usuarios': usuarios}
+        verifyEmail =Usuario.objects.filter(user__email=request.POST['email']).exclude(user__id=request.user.id).first()
+        emailUnused = verifyEmail is None
+    else:
+        usuarioForm = UserUsuarioForm(instance=usuario)
+        userForm = UserForm(instance=request.user)
+    if usuarioForm.is_valid() and userForm.is_valid() and emailUnused:
+        usuarioForm.save()
+        userForm.save()
+        message = { 'type': 'success', 'text': 'Dados atualizados com sucesso' }
+    else:
+        # Aqui verificamos se é do tipo post, para que na primeira vez que a página carregar a mensagem não apareça, já que no primeiro carregamento não enviamos um post, o form é dado como inválido e entra aqui.
+        if request.method == 'POST':
+            if emailUnused:
+            # Se o e-mail não está em uso tiver algum dado inválido.
+                message = { 'type': 'danger', 'text': 'Dados inválidos' }
+            else:
+                # Se o e-mail já está em uso por outra pessoa.
+                message = { 'type': 'warning', 'text': 'E-mail já usado' }
     usuarioForm = UserUsuarioForm(instance=usuario)
+    userForm = UserForm(instance=request.user)
     context = {
-    'usuarioForm': usuarioForm
-    }
+        'usuarioForm': usuarioForm,
+        'userForm': userForm,
+        'message': message
+        }
     return render(request, template_name='usuario/usuario-edit.html', context=context,status=200)
